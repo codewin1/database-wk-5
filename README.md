37dcdfcf5ad01ba6b80e6c4731e41c916761bb86
codewin1 <edwinmurimi19@gmail.com>
2025-10-22 21:52:28 +0300

    Solutions to the week 5 database assignment.

---
+++ README.md
@@ -0,0 +1,19 @@
+# This is the solution to the week 5 database assignnment 
+
+## Assignment Questions
+
+# Question 1
+
+Write an SQL query** to drop an index named **IdxPhone** from **customers** table.
+
+# Question 2 
+
+Write an SQL query** to create a user named **bob** with the password _'S$cu3r3!'_ , restricted to the localhost hostname.
+
+# Question 3 
+
+Write an SQL query** to grant the **INSERT** privilege to the user **bob** on the **salesDB** database.
+
+# Question 4
+
+Write an SQL query** to change the password for the user **bob** to _'P$55!23'_
---
+++ answers.sql
@@ -0,0 +1,17 @@
+-- Question 1:
+-- Write an SQL query to drop an index named IdxPhone from customers table.
+DROP INDEX IdxPhone ON customers;
+
+-- Question 2:
+-- Write an SQL query to create a user named bob with the password 'S$cu3r3!',
+-- restricted to the localhost hostname.
+CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+
+-- Question 3:
+-- Write an SQL query to grant the INSERT privilege to the user bob on the salesDB database.
+GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
+
+
+-- Question 4:
+-- Write an SQL query to change the password for the user bob to 'P$55!23'.
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
